c20816b8ee6578c63eceab24fff023d9cf8eb655
ricardo.alfredo@alumnos.uneatlantico.es <ricardo.alfredo@alumnos.uneatlantico.es>
2024-02-24 22:43:37 +0100

    Aspiradora

--- src/Modelos/Aspiradora.java
+++ src/Modelos/Aspiradora.java
@@ -2,58 +2,34 @@ import java.util.Random;
 
 public class Aspiradora {
     private int pasosRealizados;
-    private int suciedadTotal;
     private int bateria;
-    private int capacidadBolsa;
-    private int suciedadMaxima;
+    private int posicionX;
+    private int posicionY;
 
     public Aspiradora(int capacidadBolsa, int suciedadMaxima) {
-        this.capacidadBolsa = capacidadBolsa;
-        this.suciedadMaxima = suciedadMaxima;
         this.pasosRealizados = 0;
-        this.suciedadTotal = 0;
-        this.bateria = 5; 
+        this.bateria = 100;
     }
     
 
     public int getPasosRealizados( ) { 
         return pasosRealizados;}
     
-    public int getSuciedadTotal() {
-        return suciedadTotal;
-    }
-    
+   
     public int getBateria() {
         return bateria;
     }
-    
-    public int getCapacidadBolsa() {
-        return capacidadBolsa;
-    }
-    
-    public int getSuciedadMaxima() {
-        return suciedadMaxima;
-    }
+
     
     public void setPasosRealizados(int pasosRealizados) {
         this.pasosRealizados = pasosRealizados;
-    }
-    
-    public void setSuciedadTotal(int suciedadTotal) {
-        this.suciedadTotal = suciedadTotal;
     }
     
     public void setBateria(int bateria) {
         this.bateria = bateria;
     }
     
-    public void setCapacidadBolsa(int capacidadBolsa) {
-        this.capacidadBolsa = capacidadBolsa;
-    }
-    
-    public void setSuciedadMaxima(int suciedadMaxima) {
-        this.suciedadMaxima = suciedadMaxima;
-    }
+  
     
     public void limpiar(int pasos) {
         Random random = new Random();
@@ -63,19 +39,5 @@ public class Aspiradora {
             bateria--;
             if (bateria <= 0) {
                 System.out.println("Batería agotada. Deteniendo la aspiradora.");
-                break;
+                break;  
             }
-           
-            int suciedad = random.nextInt(5); 
-            suciedadTotal += suciedad;
-            if (suciedadTotal >= suciedadMaxima) {
-                System.out.println("Bolsa llena. Deteniendo la aspiradora.");
-                break;
-            }
-        }
-    }
-    public void dirigirHaciaZonaMasSucia() {
-
-        System.out.println("Dirigiendo hacia la zona más sucia...");
-    }
-}
