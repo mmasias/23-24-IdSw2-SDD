737460947ea63b0735262526985cfb582a87ebc2
JuanTocaB <juan.toca@alumnos.uneatlantico.es>
2024-02-25 18:04:34 +0100

    Constructor de la clase Elevator Creado

---
+++ PyAscensores/src/Enums/Directions.java
@@ -0,0 +1,7 @@
+package Enums;
+
+public enum Directions {
+    UP,
+    DOWN,
+    STOP
+}
--- PyAscensores/src/Models/Elevator.java
+++ PyAscensores/src/Models/Elevator.java
@@ -1,5 +1,21 @@
 package Models;
 
+import Enums.Directions;
+
 public class Elevator {
+
+    private int Id;
+    private int Capacity = 0;
+    private int CurrentFloor = 0;
+    private Directions Direction = Directions.STOP;
+    private boolean Access = true;
+
+    public Elevator(int id, int capacity, int currentFloor, Directions direction, boolean access) {
+        Id = id;
+        Capacity = capacity;
+        CurrentFloor = currentFloor;
+        Direction = direction;
+        Access = access;
+    }
 
 }
