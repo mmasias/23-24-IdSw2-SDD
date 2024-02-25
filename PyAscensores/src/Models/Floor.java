bae377ede3c930e1c4345640f8f90479483eee7c
EnriqueTorralboRamos <102521084+EnriqueTorralboRamos@users.noreply.github.com>
2024-02-25 19:55:11 +0100

    Floor model created

--- PyAscensores/src/Models/Floor.java
+++ PyAscensores/src/Models/Floor.java
@@ -1,5 +1,40 @@
 package Models;
 
+import java.util.List;
+
 public class Floor {
+    String id;
+    List<Person> people;
+    List<Person> waitingPeople;
+    boolean acces;
+    public Floor(String id, boolean acces, List<Person> people, List<Person> waitingPeople){
+        this.id=id;
+        this.acces=acces;
+        this.people=people;
+        this.waitingPeople=waitingPeople;       
+
+    }
+
+    public String getId() {
+        return id;
+    }
+    public List<Person> getPeople() {
+        return people;
+    }
+    public List<Person> getWaitingPeople() {
+        return waitingPeople;
+    }
+    public boolean getAccess() {
+        return acces;
+    }
+    public void setAcces(boolean acces) {
+        this.acces = acces;
+    }
+    public void setPeople(List<Person> people) {
+        this.people = people;
+    }
+    public void setWaitingPeople(List<Person> waitingPeople) {
+        this.waitingPeople = waitingPeople;
+    }
 
 }
