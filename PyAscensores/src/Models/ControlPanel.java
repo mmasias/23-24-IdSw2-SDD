0ce4b179b09c3a8cfcb54225377ec08063352791
Jesús Ramos <47223013+jramsgz@users.noreply.github.com>
2024-02-26 21:54:34 +0000

    Add ControlPanel methods

--- PyAscensores/src/Models/ControlPanel.java
+++ PyAscensores/src/Models/ControlPanel.java
@@ -1,5 +1,51 @@
 package Models;
 
 public class ControlPanel {
+    private List<Integer> floorsToStop;
+    private List<Integer> floorsToGo;
 
+    public ControlPanel(List<Integer> floorsToStop, List<Integer> floorsToGo) {
+        this.floorsToStop = floorsToStop;
+        this.floorsToGo = floorsToGo;
+    }
+
+    public List<Integer> getfloorsToStop() {
+        return floorsToStop;
+    }
+
+    public List<Integer> getfloorsToGo() {
+        return floorsToGo;
+    }
+
+    public void addFloorToStop(int floor) {
+        this.floorsToStop.add(floor);
+    }
+
+    public void addFloorToGo(int floor) {
+        this.floorsToGo.add(floor);
+    }
+
+    public void removeFloorToStop(int floor) {
+        this.floorsToStop.remove(floor);
+    }
+
+    public void removeFloorToGo(int floor) {
+        this.floorsToGo.remove(floor);
+    }
+
+    public void clearFloorsToStop() {
+        this.floorsToStop.clear();
+    }
+
+    public void clearFloorsToGo() {
+        this.floorsToGo.clear();
+    }
+
+    public boolean hasFloorsToStop() {
+        return !this.floorsToStop.isEmpty();
+    }
+
+    public boolean hasFloorsToGo() {
+        return !this.floorsToGo.isEmpty();
+    }
 }
