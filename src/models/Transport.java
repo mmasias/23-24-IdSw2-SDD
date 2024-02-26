62ef506946695697c4553770dff22b4c4638c9aa
recinosjer <69140645+recinosjer@users.noreply.github.com>
2024-02-26 23:50:02 +0100

    Class Tile and Transport with enums

---
+++ src/models/Transport.java
@@ -0,0 +1,21 @@
+package models;
+
+public class Transport {
+    private TransportTypes type;
+
+    public Transport(TransportTypes type) {
+        this.type = type;
+    }
+
+    public TransportTypes getType() {
+        return type;
+    }
+
+    public int getSpeed() {
+        return type.getSpeed();
+    }
+
+    public String getAsciiSymbol() {
+        return type.getAsciiSymbol();
+    }
+}
