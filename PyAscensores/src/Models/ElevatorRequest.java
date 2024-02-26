6254da1145a5480b2a119f775d71a3a57e4bfd7b
adrian.ortiz <adrian.ortiz@alumnos.uneatlantico.es>
2024-02-26 08:28:11 +0100

    Clase ElevatorRequest hecha

--- PyAscensores/src/Models/ElevatorRequest.java
+++ PyAscensores/src/Models/ElevatorRequest.java
@@ -1,5 +1,19 @@
 package Models;
 
-public class ElevatorRequest {
+import java.util.List;
 
+public class ElevatorRequest {
+    private List<Integer> floorsToStop;
+
+    public ElevatorRequest(List<Integer> floorsToStop) {
+        this.floorsToStop = floorsToStop;
+    }
+
+    public List<Integer> getfloorsToStop() {
+        return floorsToStop;
+    }
+
+    public void setfloorsToStop(List<Integer> floorsToStop) {
+        this.floorsToStop = floorsToStop;
+    }
 }
