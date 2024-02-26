b25f344dd4b776ab2ba411daece617ff71251741
DiegoSainzPardo <102520932+DiegoSainzPardo@users.noreply.github.com>
2024-02-26 19:23:10 +0100

    Building Java
    Clase Building; Setters y Getters

--- PyAscensores/src/Models/Building.java
+++ PyAscensores/src/Models/Building.java
@@ -1,5 +1,56 @@
 package Models;
+import java.util.List;
 
-public class Building {
+public class Building 
+{
+    List<Floor>    _floor;
+    List<Elevator> _elevator;
+    boolean        _access;
+    int            _capacity;
+    
+    //region Setters
+    public void SetCapacity(int capacity)
+    {
+        _capacity = capacity; 
+    }
 
+    public void SetAccess(boolean access)
+    {
+        _access = access;
+    }
+
+    public void SetFloors(List<Floor> floor)
+    {
+        _floor = floor;
+    }
+
+    public void SetElevators(List<Elevator> elevator)
+    {
+        _elevator = elevator;
+    }
+    //endregion
+
+    //region Getters
+    public int GetCapacity()
+    {
+        return _capacity;
+    }
+
+    public boolean GetAccess()
+    {
+        return _capacity;
+    }
+
+    public List<Floor> GetFloors()
+    {
+        return _floor;
+    }
+
+    public List<Elevator> GetElevators()
+    {
+        return _elevator;
+    }
+
+  
+    //endregion
 }
