389b0238219c5dab391cf2b0ac6f56fe743536ac
Veronika Esparragoza <83025946+VeronikaEspa@users.noreply.github.com>
2024-02-27 23:33:58 +0100

    añadiendo estructura inicial del controlador de movimiento

---
+++ src/controllers/MovementController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+public class MovementController {
+    public void moveCharacter(){}
+
+    private void getMovementFromInput(){}
+
+    private void calculateNewPosition(){}
+
+    private void adjustPositionWithinBounds(){}
+
+    private void isPositionValid(){}
+}
---
+++ src/controllers/RandomMovementController.java
@@ -0,0 +1,5 @@
+package controllers;
+
+public class MovementController {
+    public void getRandomMovement(){}
+}
