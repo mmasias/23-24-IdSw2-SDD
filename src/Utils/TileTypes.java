5350629a558fe70feb9a1e7666d35d3851d55164
Hexcss <cadersuay.javier@gmail.com>
2024-02-27 23:16:34 +0100

    fix: TileTypes Colours

--- src/Utils/TileTypes.java
+++ src/Utils/TileTypes.java
@@ -1,13 +1,14 @@
+package Utils;
 public enum TileTypes {
-    Floor(1, "Color1", "Symbol1"),
-    Sand(1, "Color2", "Symbol2"),
-    Wall(0, "Color3", "Symbol3"),
+    Floor(0, "Color1", "Symbol1"),
+    Sand(6, "Color2", "Symbol2"),
+    Wall(8, "Color3", "Symbol3"),
     ShortGrass(1, "Color4", "Symbol4"),
     MediumGrass(2, "Color5", "Symbol5"),
-    TallGrass(1, "Color6", "Symbol6"),
-    Water(1, "Color7", "Symbol7"),
-    DeepWater(2, "Color8", "Symbol8"),
-    Mountain(1, "Color9", "Symbol9");
+    TallGrass(3, "Color6", "Symbol6"),
+    Water(4, "Color7", "Symbol7"),
+    DeepWater(5, "Color8", "Symbol8"),
+    Mountain(7, "Color9", "Symbol9");
 
     private final int tileNumber;
     private final String asciiColor;
