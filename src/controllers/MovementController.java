440770cbcbecc7bd4acd87f8ac2bb2a31a6b93fd
Veronika Esparragoza <vesparragozam@gmail.com>
2024-02-28 00:25:19 +0100

    añadiendo test del randomController e inputController

--- src/controllers/MovementController.java
+++ src/controllers/MovementController.java
@@ -2,12 +2,8 @@ package controllers;
 
 public class MovementController {
     public void moveCharacter(){}
-
     private void getMovementFromInput(){}
-
     private void calculateNewPosition(){}
-
     private void adjustPositionWithinBounds(){}
-
     private void isPositionValid(){}
 }
--- src/controllers/RandomMovementController.java
+++ src/controllers/RandomMovementController.java
@@ -1,5 +1,30 @@
 package controllers;
 
-public class MovementController {
-    public void getRandomMovement(){}
+import utils.Movement;
+
+import java.util.Random;
+
+public class RandomMovementController {
+    Random random = new Random();
+    int randomNumber = random.nextInt(4);
+    public void getRandomMovement(){
+        switch (randomNumber){
+            case 0:
+                System.out.println(Movement.UP.getKey());
+                break;
+            case 1:
+                System.out.println(Movement.DOWN.getKey());
+                break;
+            case 2:
+                System.out.println(Movement.LEFT.getKey());
+                break;
+            case 3:
+                System.out.println(Movement.RIGHT.getKey());
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        new RandomMovementController().getRandomMovement();
+    }
 }
