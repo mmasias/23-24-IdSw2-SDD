fa0656467e6bb9397d389a8909e2918fd8b3bfb5
JuanTocaB <juan.toca@alumnos.uneatlantico.es>
2024-02-28 13:38:45 +0100

    Añadir renderización de edificio en BuildingView y eliminar PersonView

--- PyAscensores/src/Views/BuildingView.java
+++ PyAscensores/src/Views/BuildingView.java
@@ -1,9 +1,37 @@
 package Views;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import Models.Building;
+import Models.Elevator;
+import Models.Floor;
 
 public class BuildingView {
-    public static String render(Building building) {
-        return "";
+
+    private List<Floor> Floors;
+    private List<Elevator> Elevators;
+    private ArrayList<String[]> Building;
+
+    public BuildingView(Building building) {
+        Floors = building.GetFloors();
+        Elevators = building.GetElevators();
+        Building = new ArrayList<String[]>();
     }
+
+    public void render() {
+        for (Elevator e : Elevators) {
+            String eView = ElevatorView.render(e, Floors.size());
+            String eViewSplit[] = eView.split("\n");
+            Building.add(eViewSplit);
+        }
+
+        for (int i = 0; i < Building.get(0).length; i++) {
+            for (int j = 0; j < Building.size(); j++) {
+                System.out.print(" " + Building.get(j)[i]);
+            }
+            System.out.println();
+        }
+    }
+
 }
