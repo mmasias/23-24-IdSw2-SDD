9a6b646bcb3c40ab9c87a280d8601cb9ada9bd5e
JuanTocaB <juan.toca@alumnos.uneatlantico.es>
2024-02-28 18:04:45 +0100

    Agrega métodos para añadir ascensores y pisos al edificio

--- PyAscensores/src/Views/BuildingView.java
+++ PyAscensores/src/Views/BuildingView.java
@@ -20,18 +20,36 @@ public class BuildingView {
     }
 
     public void render() {
-        for (Elevator e : Elevators) {
-            String eView = ElevatorView.render(e, Floors.size());
-            String eViewSplit[] = eView.split("\n");
-            Building.add(eViewSplit);
-        }
-
+        addElevatorToBuilding();
+        addFloorToBuilding();
         for (int i = 0; i < Building.get(0).length; i++) {
             for (int j = 0; j < Building.size(); j++) {
                 System.out.print(" " + Building.get(j)[i]);
             }
             System.out.println();
         }
+    }
+
+    private void addElevatorToBuilding() {
+        for (Elevator elevator : Elevators) {
+            String View = ElevatorView.render(elevator, Floors.size());
+            String ViewSplit[] = View.split("\n");
+            Building.add(ViewSplit);
+        }
+    }
+
+    private void addFloorToBuilding() {
+        addLeftWing();
+        addRightWing();
+        // TODO: Implement this method
+    }
+
+    private void addLeftWing() {
+        // TODO: Implement this method
+    }
+
+    private void addRightWing() {
+        // TODO: Implement this method
     }
 
 }
