0e19fa869edae52ed8643bf45b03dfa1c615d160
luciaZama <lzamanillol@gmail.com>
2024-03-05 23:10:17 +0100

    FileReaderController arreglado

--- src/Controllers/FileReaderController.java
+++ src/Controllers/FileReaderController.java
@@ -1,7 +1,7 @@
 package Controllers;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
+import java.io.*;
 import java.util.*;
 
 class FileReaderController {
@@ -16,48 +16,32 @@ class FileReaderController {
         this.filePath = path;
     }
 
-    public List<String[]> readCSV() throws FileNotFoundException {
+   
+    public List<String[]> readCSV() throws FileNotFoundException  {
         List<String[]> listTiles = new ArrayList<String[]>();
-        String[] tilesString = new String[0];
-        int count = 0;
-
-        FileReader fr = null;
+        
+        
         try {
-            fr = new FileReader(filePath);
-            int character = fr.read();
 
-            while (character != -1) {
-                String tileNumber = String.valueOf(character);
-                tilesString[count] = tileNumber;
+            File file = new File(filePath);
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
 
-                count++;
-                character = fr.read();
+            String line;
+            
+            while((line = br.readLine()) != null) {
+                String[] tokens = line.split(",", 0);
+                listTiles.add(tokens);
+                
             }
 
-            System.out.println(listTiles);
-        } catch (FileNotFoundException e) {
-
-            System.out.println("File not found");
-
-            System.out.println(e.getMessage());
-
-            throw new FileNotFoundException(e.getMessage());
         } catch (Exception e) {
 
             System.out.println("Failed to read file");
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (Exception e) {
-                System.out.println("Failed to close the file");
-                System.out.println(e.getMessage());
-            }
         }
 
-        listTiles.add(tilesString);
         return listTiles;
     }
+
 }
