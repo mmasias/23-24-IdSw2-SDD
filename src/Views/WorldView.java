9f2adef66072f794bdecee07b4c754ff2e32eca8
Juan José Cobo Cano <juan.cobo@alumnos.uneatlantico.es>
2024-04-06 17:49:11 +0200

    Elaboración de la clase WorldView (por consola por el momento)

--- src/Views/WorldView.java
+++ src/Views/WorldView.java
@@ -1,5 +1,46 @@
 package Views;
 
+import Models.*;
+
 public class WorldView {
 
+    public void displayWorld(World world) {
+        cleanScreen();
+        displayTime(world.getTime());
+        displayMap(world.getMap(), world.getEntities());
+    }
+
+    public void cleanScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public void displayTime(Time time) {
+        System.out.println("Hora actual: " + time.getCurrentTime() + " - " + time.getTimeOfDay());
+    }
+
+    public void displayMap(Map map, List<Entity> entities) {
+        Tile[][] tiles = map.getTiles();
+        String[][] displayMatrix = new String[map.getHeight()][map.getWidth()];
+
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles[i].length; j++) {
+                displayMatrix[i][j] = tiles[i][j].getAsciiSymbol();
+            }
+        }
+
+        for (Entity entity : entities) {
+            int x = entity.getPosition().getX();
+            int y = entity.getPosition().getY();
+
+            displayMatrix[y][x] = entity.getCurrentTransport().getAsciiSymbol();
+        }
+
+        for (String[] row : displayMatrix) {
+            for (String symbol : row) {
+                System.out.print(symbol + " ");
+            }
+            System.out.println();
+        }
+    }
 }
