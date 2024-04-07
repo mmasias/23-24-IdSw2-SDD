758948850633ea2263ed7d45447efbd2203846d1
PabloRuizz <pabloruiz6@gmail.com>
2024-04-07 17:57:46 +0200

    Controlador de personas

--- PyAscensores/src/Controllers/People.java
+++ PyAscensores/src/Controllers/People.java
@@ -5,6 +5,36 @@ import Models.Person;
 
 public class People {
 
+    private ArrayList<Person> peopleList;
+
+    public People() {
+        this.peopleList = new ArrayList<>();
+    }
+
+    public void create(Person person) {
+        this.peopleList.add(person);
+    }
+
+    public void update(int index, Person updatedPerson) {
+        if (index >= 0 && index < this.peopleList.size()) {
+            this.peopleList.set(index, updatedPerson);
+        } else {
+            System.out.println("Índice fuera de rango.");
+        }
+    }
+
+    public void delete(int index) {
+        if (index >= 0 && index < this.peopleList.size()) {
+            this.peopleList.remove(index);
+        } else {
+            System.out.println("Índice fuera de rango.");
+        }
+    }
+
+    public ArrayList<Person> index() {
+        return this.peopleList;
+    }
+
     public static ArrayList<Person> createPeople(int amount, int currentFloor, int destination) {
         ArrayList<Person> people = new ArrayList<Person>();
 
@@ -18,8 +48,7 @@ public class People {
     }
 
     private static int getTimeOnFloor() {
-        // TODO: Implement randomization
+        // TODO: Implementar la aleatorización
         return 5;
     }
-
 }
