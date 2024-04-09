30dac6f6c2284cd2d197b683ccd8dfe89874fdb6
luciaZama <lzamanillol@gmail.com>
2024-04-09 20:42:54 +0200

    Tile class finished

--- src/Models/Tile.java
+++ src/Models/Tile.java
@@ -1,5 +1,16 @@
 package Models;
-
+import Enums.TileTypes;
 public class Tile {
-    
+    private TileTypes type;
+    public Tile(TileTypes type) {
+        this.type = type;
+    }
+
+    public void changeType(TileTypes newType) {
+        this.type = newType;
+    }
+
+    public TileTypes getType() {
+        return type;
+    }
 }
