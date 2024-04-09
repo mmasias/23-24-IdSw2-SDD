dad1c39f232f41d2d9353c2ed66f5552c3f74f86
recinosjer <69140645+recinosjer@users.noreply.github.com>
2024-04-09 22:12:34 +0200

    Move from Legacy to src Tile.java

--- src/Models/Tile.java
+++ src/Models/Tile.java
@@ -1,5 +1,27 @@
 package Models;
 
+import Enums.TileTypes;
+
 public class Tile {
-    
+    private TileTypes type;
+
+    public Tile(TileTypes type) {
+        this.type = type;
+    }
+
+    public TileTypes getType() {
+        return type;
+    }
+
+    public String getAsciiSymbol() {
+        return type.getAsciiSymbol();
+    }
+
+    public String getAsciiColor() {
+        return type.getAsciiColor();
+    }
+
+    public void setType(TileTypes type) {
+        this.type = type;
+    }
 }
