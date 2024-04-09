9484010509ff12bf27517384a093de6d8321f635
Veronika Esparragoza <vesparragozam@gmail.com>
2024-04-09 09:55:37 +0200

    Creando la rama para algunos metodos del worldController

--- src/Controllers/WorldController.java
+++ src/Controllers/WorldController.java
@@ -1,5 +1,25 @@
 package Controllers;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WorldController {
-    
+
+    private void InitializeWorldMap(){
+
+    }
+
+    private void getRandomCharacterMovement(){
+
+    }
+
+    private char getUserInput(){
+        return input;
+
+    }
+
+    private void processUserInput(char input){
+
+    }
+
 }
