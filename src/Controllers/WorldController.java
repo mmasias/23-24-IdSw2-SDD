08411802f971d0623c08689c5f905f8a3fa8c1c4
Veronika Esparragoza <vesparragozam@gmail.com>
2024-04-09 15:59:45 +0200

    Añadiendo los metodos getUserInput y getRandomCharacterMovement del WorldController

--- src/Controllers/WorldController.java
+++ src/Controllers/WorldController.java
@@ -1,6 +1,6 @@
 package Controllers;
 
-import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class WorldController {
@@ -10,16 +10,22 @@ public class WorldController {
     }
 
     private void getRandomCharacterMovement(){
-
+        Random random = new Random();
+        char[] movements = {'W', 'A', 'S', 'D'};
+        processUserInput(movements[random.nextInt(movements.length)]);
     }
 
-    private char getUserInput(){
-        return input;
-
+    private void getUserInput(){
+        Scanner scanner = new Scanner(System.in);
+        char direction = Character.toUpperCase(scanner.nextLine().charAt(0));
+        scanner.close();
+        processUserInput(direction);
     }
 
     private void processUserInput(char input){
-
+        if( input == ('W' | 'A' | 'S' | 'D')){
+            // Move character
+        }
     }
 
 }
