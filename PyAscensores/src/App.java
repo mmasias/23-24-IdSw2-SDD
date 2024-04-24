01e668f7cfd91a3a8db50d27d081f81218a581dd
JuanTocaB <juan.toca@alumnos.uneatlantico.es>
2024-04-24 20:04:19 +0200

    Actualizar valores iniciales y simulación

--- PyAscensores/src/App.java
+++ PyAscensores/src/App.java
@@ -1,8 +1,10 @@
+import Controllers.InitialValues;
 import Controllers.Simulation;
 
 public class App {
     public static void main(String[] args) {
-        Simulation simulation = new Simulation();
+        InitialValues values = new InitialValues(5, 3);
+        Simulation simulation = new Simulation(values);
         simulation.start();
     }
 }
