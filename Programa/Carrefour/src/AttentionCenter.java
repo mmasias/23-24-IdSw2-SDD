6e486b17f4007114191c316fe558ba9038d5c60f
MOROMEM <sergio.moro@alumnos.uneatlantico.es>
2024-05-02 13:32:03 +0200

    Actualizacion java

--- Programa/Carrefour/src/AttentionCenter.java
+++ Programa/Carrefour/src/AttentionCenter.java
@@ -1,11 +1,34 @@
-import java.util.HashMap;
-import java.util.Map;
 
-public class AttentionCenter {
+public class AttentionCenter implements TimeObserver {
 
+    public  void onTimeChange(String time, boolean allOpen){
 
-    Map<Integer, Integer> emptyCasRegisterMap = new HashMap<>();
+        Queue customerQueue = Queue.getInstance(); // Obtiene la instancia de la cola
 
-    //Aqui extraer lista de cajas para verificar si estan ocupadas o no y poder mandar cola de customers
-    
+        while (customerQueue.GetSize() > 0) {
+            
+            Customer customer = customerQueue.Dequeue(); // Extrae un cliente de la cola
+            CashRegister availableRegister = findAvailableRegister();
+            if (availableRegister != null) {
+                availableRegister.remainingPacks = customer.numberOfPacks;
+            } 
+            if (customerQueue.GetSize()> 15) {
+                availableRegister.OpenNewCash();
+            } else {
+                System.out.println("No hay cajas disponibles en este momento.");
+                break; // Sale del bucle si no hay cajas disponibles
+            }
+        }
+    }
+
+    private CashRegister findAvailableRegister() {
+        for (CashRegister register : CashRegister.lRegisters) {
+            if (!register.isOccupied && register.isOpen) {
+                
+                return register;
+            }
+        }
+        return null; // Retorna null si no hay cajas libres
+    }
 }
+
--- Programa/Carrefour/src/Queue.java
+++ Programa/Carrefour/src/Queue.java
@@ -1,15 +1,33 @@
 import java.util.LinkedList;
 
 public class Queue {
+    private static Queue instance;
+    private LinkedList<Customer> queue;
 
-    LinkedList<Customer> queue;
+    private Queue() {
+        this.queue = new LinkedList<>();
+    }
 
-    public Queue() {this.queue = new LinkedList<>();}
+    public static Queue getInstance() {
+        if (instance == null) {
+            instance = new Queue();
+        }
+        return instance;
+    }
 
-    public void Enqueue(Customer customer) {queue.addLast(customer);}
+    public void Enqueue(Customer customer) {
+        queue.addLast(customer);
+    }
 
-    public void Dequeue(){queue.pollFirst();}
+    public Customer Dequeue() {
+        return queue.pollFirst();
+    }
 
-    public int GetSize() {return queue.size();}
-    
+    public int GetSize() {
+        return queue.size();
+    }
+
+    public LinkedList<Customer> GetQueue() {
+        return queue;
+    }
 }
