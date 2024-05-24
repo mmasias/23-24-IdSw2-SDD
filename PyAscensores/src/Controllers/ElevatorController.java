e5e7076bd0046ef06b88ccf23d9c46cf0c1dc0b0
EnriqueTorralboRamos <102521084+EnriqueTorralboRamos@users.noreply.github.com>
2024-05-24 21:25:17 +0200

    edit ElevatorController
    reset person destination and timeOnFloor
    make a FloorRequest when a person enter in an elevator

--- PyAscensores/src/Controllers/ElevatorController.java
+++ PyAscensores/src/Controllers/ElevatorController.java
@@ -2,30 +2,30 @@ package Controllers;
 
 import java.util.ArrayList;
 import Lists.ElevatorList;
-import Models.Elevator;
-import Models.ElevatorRequest;
+import Models.*;
 import Enums.Direction;
-import Models.Building;
-import Models.Person;
-import Models.Floor;
+
 
 public class ElevatorController {
+    Building building;
+    InitialValues initialValues= new InitialValues(5,2);
 
-    public void update(Building Building) {
-        updatePosition(Building);
-        updatePeople(Building);
+    public void update(Building building) {
+        this.building = building;
+        updatePosition();
+        updatePeople();
     }
 
-    private void updatePeople(Building building) {
+    private void updatePeople() {
         for (int i = 0; i < building.getElevators().size(); i++) {
             Elevator elevator = building.getElevators().get(i);
             if (elevator.getDirection() == Direction.STOP) {
-                updateElevatorPeople(building, elevator);
+                updateElevatorPeople(elevator);
             }
         }
     }
 
-    private void updateElevatorPeople(Building building, Elevator elevator) {
+    private void updateElevatorPeople(Elevator elevator) {
         for (int i = 0; i < building.getFloors().size(); i++) {
             Floor floor = building.getFloors().get(i);
             processWaitingPeople(elevator, floor);
@@ -36,7 +36,7 @@ public class ElevatorController {
     private void processWaitingPeople(Elevator elevator, Floor floor) {
         for (int i = 0; i < floor.getWaitingPeople().size(); i++) {
             Person personOnFloor = floor.getWaitingPeople().get(i);
-            if (elevatorHasSpace(elevator) && elevator.getAccess() == true) {
+            if (elevatorHasSpace(elevator) && elevator.getAccess()) {
                 movePersonToElevator(elevator, floor, personOnFloor);
             }
         }
@@ -58,21 +58,24 @@ public class ElevatorController {
     private void movePersonToElevator(Elevator elevator, Floor floor, Person person) {
         floor.removeWaitingPerson(person.getId());
         elevator.addPeopleInside(person);
+        building.getControlPanel().addFloorRequest(new FloorRequest(person.getDestination(), elevator.getId()));
     }
 
     private void removePersonFromElevator(Elevator elevator, Floor floor, Person person) {
         elevator.removePeopleInside(person.getId());
+        person.setDestination(initialValues.getRandomFloor());
+        person.setTimeOnFloor(initialValues.getRandomTimeOnFloor(0,100));
         floor.addPersonOnFloor(person);
     }
 
-    private void updatePosition(Building building) {
+    private void updatePosition() {
         for (int i = 0; i < building.getElevators().size(); i++) {
             Elevator elevator = building.getElevators().get(i);
-            if (elevator.getDirection().equals(Direction.DOWN) && elevator.getAccess() == true) {
+            if (elevator.getDirection().equals(Direction.DOWN) && elevator.getAccess()) {
                 elevator.setCurrentFloor(elevator.getCurrentFloor() - 1);
-            } else if (elevator.getDirection().equals(Direction.UP) & elevator.getAccess() == true) {
+            } else if (elevator.getDirection().equals(Direction.UP) && elevator.getAccess()) {
                 elevator.setCurrentFloor(elevator.getCurrentFloor() + 1);
-            } else if (elevator.getDirection().equals(Direction.STOP) & elevator.getAccess() == true) {
+            } else if (elevator.getDirection().equals(Direction.STOP) && elevator.getAccess()) {
                 elevator.setCurrentFloor(elevator.getCurrentFloor());
             }
         }
