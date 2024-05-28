b7243419202b8928fef647ba3e88f69a6bce43f1
DiegoSainzPardo <102520932+DiegoSainzPardo@users.noreply.github.com>
2024-05-28 10:51:11 +0200

    Personas esperando y personas en Planta

--- PyAscensores/src/Views/BuildingView.java
+++ PyAscensores/src/Views/BuildingView.java
@@ -41,6 +41,12 @@ public class BuildingView {
 
     private String mergeViews(ArrayList<String[]> views, Building building) {
         StringBuilder mergedViews = new StringBuilder();
+        
+        mergedViews.append(appendTextWithSpacesBeforeAndAfter(9, 20, "Personas"));
+        mergedViews.append(appendTextWithSpacesBeforeAndAfter(0, 0, "Personas\n"));
+        mergedViews.append(appendTextWithSpacesBeforeAndAfter(9, 20, "Esperando"));
+        mergedViews.append(appendTextWithSpacesBeforeAndAfter(0, 0, "en Planta\n"));
+        
         for (int i = 0; i < building.getFloors().size(); i++) {
             for (String[] view : views) {
                 mergedViews.append(view[i] + "  ");
@@ -49,5 +55,21 @@ public class BuildingView {
         }
 
         return mergedViews.toString();
+    }
+
+    private String appendTextWithSpacesBeforeAndAfter(int numberOfSpacesBefore, int numberOfSpacesAfter, String text) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < numberOfSpacesBefore; i++) {
+            sb.append(" ");
+        }
+        
+        sb.append(text);
+        
+        for (int i = 0; i < numberOfSpacesAfter; i++) {
+            sb.append(" ");
+        }
+        
+        return sb.toString();
     }
 }
